bbba353fcc47f2cb2e80103020e1b8d751eb6d93
afgh <augustogutierrezh@gmail.com>
2024-07-28 01:19:35 -0500

    Se actualiza la ruta de personalPortfolio_API

--- Personal_Portfolio_Frontend/src/services/config.js
+++ Personal_Portfolio_Frontend/src/services/config.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "https://66318dfdc51e14d69561f522.mockapi.io/contact_form"
-export const BASE_URL = "http://127.0.0.1:8000/api/v1/"
+export const BASE_URL = "https://personal-portfolio-g18.onrender.com/api/v1/"
